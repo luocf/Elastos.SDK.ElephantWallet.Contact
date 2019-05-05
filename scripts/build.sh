0a18785083231f0af2154ff1316b729afab51553
mengxk <xiaokun.meng@qcast.cn>
2019-05-05 14:53:57 +0800

    init and add submodules.

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+CURRENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
+PROJECT_DIR=$(dirname "$CURRENT_DIR")
+DEPENDS_DIR="$PROJECT_DIR/config";
+
+cd "$PROJECT_DIR";
+git submodule init;
+git submodule update;
+
+build_extfunc_depends()
+{
+	"$DEPENDS_DIR/scripts/build-curl.sh" $@;
+    if [[ $CFG_TARGET_PLATFORM == "iOS" ]]; then
+        loginfo "Ignore to build sqlite for iOS, use buildin."
+    else
+        "$DEPENDS_DIR/scripts/build-sqlite.sh" $@;
+    fi
+
+	#CFG_BUILD_ROOT
+	"$DEPENDS_DIR/scripts/build-Elastos.SDK.Keypair.C.sh" $@;
+	
+}
+
+export CFG_PROJECT_NAME="Elastos.SDK.Contact";
+export CFG_PROJECT_DIR="$PROJECT_DIR";
+source "$DEPENDS_DIR/scripts/build.sh" $@;
+
